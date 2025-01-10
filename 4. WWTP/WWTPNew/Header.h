fd41d05d5a621f3162b8a5c04c02aac6c021ae48
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2025-01-10 18:23:26 +0700

    Update Header.h

--- 4. WWTP/WWTPNew/Header.h
+++ 4. WWTP/WWTPNew/Header.h
@@ -9,9 +9,9 @@ const uint8_t PIN_TRANSFER_PUMP = 8;  // In4 - transfer pump
 const uint8_t EEPROM_ADDR_DELAY1 = 0;
 const uint8_t EEPROM_ADDR_DELAY2 = 1;
 
-const unsigned long FILLING_TIMEOUT = 250000;   // 250 seconds
-const unsigned long TRANSFER_TIMEOUT = 380000;  // 380 seconds
+const uint32_t FILLING_TIMEOUT = 250000;   // 250 seconds
+const uint32_t TRANSFER_TIMEOUT = 380000;  // 380 seconds
 const uint16_t VOLTAGE_SAMPLES = 500;
 const uint16_t RADAR_SAMPLES = 200;
 const float VOLTAGE_THRESHOLD = 500.0;
-const unsigned long VERIFICATION_TIME = 15000;
+const uint32_t VERIFICATION_TIME = 15000;

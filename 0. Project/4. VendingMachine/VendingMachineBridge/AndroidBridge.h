5551ad28727b0dfa1f5c1022e957114f3352b34c
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2025-02-18 12:33:58 +0700

    .

---
+++ 0. Project/4. VendingMachine/VendingMachineBridge/AndroidBridge.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Arduino.h"
+#include "EEPROM.h"
+
+class AndroidBridge {
+public:
+  AndroidBridge(Stream* androidSerial, Stream* debugSerial);
+  void begin();
+  void handleCommands();
+  void sendResponse(const String& response);
+  int getReceivedCommand();
+  bool isUnlocking();
+  String getOTAddress();
+  void writeOTAddress(const String& address);
+
+private:
+  Stream* android;
+  Stream* debug;
+  int lastCommand;
+  bool unlockRequested;
+  String OTAddress;
+  static const int OT_ADDRESS_LOCATION = 0;  
+  static const int OT_ADDRESS_LENGTH = 6;
+};
---
+++ 0. Project/4. VendingMachine/VendingMachineBridge/VendingMachineBridge.ino
@@ -0,0 +1,27 @@
+#include "AndroidBridge.h"
+#include "DriverBridge.h"
+#include "AltSoftSerial.h"
+
+AltSoftSerial altSerial;
+
+AndroidBridge android(&Serial, nullptr);
+DriverBridge driver(&altSerial, nullptr);
+
+void setup() {
+  Serial.begin(9600);
+  altSerial.begin(9600);
+  android.writeOTAddress("OT0001");
+}
+
+void loop() {
+  android.handleCommands();
+
+  if (android.isUnlocking()) driver.openLock();
+
+  int command = android.getReceivedCommand();
+  if (command >= 0) {
+    bool status = driver.executeMotorCommand(command);
+    if (status) android.sendResponse("a");
+    else android.sendResponse("b");
+  }
+}

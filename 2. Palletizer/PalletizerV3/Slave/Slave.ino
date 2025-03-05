07ff24ec4686f7ce0d3f6ae137fc084e9964ccb5
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2025-03-05 13:42:06 +0700

    .

--- 2. Palletizer/PalletizerV3/Slave/Slave.ino
+++ 2. Palletizer/PalletizerV3/Slave/Slave.ino
@@ -2,25 +2,32 @@
 
 #include "StepperSlave.h"
 
+#define SLAVE_ADDR 'x'
 #define CLK_PIN 10
 #define CW_PIN 11
 #define EN_PIN 12
 #define RX_PIN 8
 #define TX_PIN 9
 #define SENSOR_PIN 6
+
+#if SLAVE_ADDR == 't'
+#define BRAKE_PIN -1  // 12
+#else
 #define BRAKE_PIN -1
+#endif
+
 #define INVERT_BRAKE false
 #define INDICATOR_PIN 13
 
-StepperSlave slave('x', RX_PIN, TX_PIN, CLK_PIN, CW_PIN, EN_PIN, SENSOR_PIN, BRAKE_PIN, INVERT_BRAKE, INDICATOR_PIN);
+StepperSlave slave('t', RX_PIN, TX_PIN, CLK_PIN, CW_PIN, EN_PIN, SENSOR_PIN, BRAKE_PIN, INVERT_BRAKE, INDICATOR_PIN);
 
-// x;1;0
-// x;1;2000;d2000;0
-// x;1;2000;0;3000;0;4000
+// t;1;0
+// t;1;2000;d2000;0
+// t;1;2000;0;3000;0;4000
 
 // CMD_NONE     = 0
-// CMD_STAR     = 1 // x;1;200
-// CMD_ZERO     = 2 // x;2
+// CMD_STAR     = 1 // t;1;200
+// CMD_ZERO     = 2 // t;2
 // CMD_PAUSE    = 3
 // CMD_RESUME   = 4
 // CMD_RESET    = 5

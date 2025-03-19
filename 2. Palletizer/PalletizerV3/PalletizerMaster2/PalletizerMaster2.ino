afea8bca1ee6d592feb0c01f62a92c9cffb8fc4b
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2025-03-19 08:34:16 +0700

    updates

---
+++ 2. Palletizer/PalletizerV3/PalletizerMaster2/PalletizerMaster2.ino
@@ -0,0 +1,17 @@
+#define ENABLE_MODULE_SERIAL_ENHANCED
+
+#include "PalletizerMaster.h"
+
+#define RX_PIN 8
+#define TX_PIN 9
+#define INDICATOR_PIN 2
+
+PalletizerMaster master(RX_PIN, TX_PIN, INDICATOR_PIN);
+
+void setup() {
+  master.begin();
+}
+
+void loop() {
+  master.update();
+}

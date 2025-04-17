12de896d9193705626e6129e3face75279fd7308
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2025-04-17 15:41:48 +0700

    Update StepperSlave.cpp

--- 0. Project Final/5. Palletizer/PalletizerV6/PalletizerSlave/StepperSlave.cpp
+++ 0. Project Final/5. Palletizer/PalletizerV6/PalletizerSlave/StepperSlave.cpp
@@ -27,7 +27,7 @@ StepperSlave::StepperSlave(
 void StepperSlave::begin() {
   Serial.begin(9600);
   masterCommSerial.begin(9600);
-  masterSerial.begin(&masterCommSerial);  
+  masterSerial.begin(&masterCommSerial);
   debugSerial.begin(&Serial);
   masterSerial.setDataCallback(onMasterDataWrapper);
 

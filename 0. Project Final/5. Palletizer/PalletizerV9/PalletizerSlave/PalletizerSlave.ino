fcf3841bc6ab23ddde911e90e3c49122eaf0494b
Iwan Dwi Purwanto <mcclawsher369@gmail.com>
2025-05-26 19:42:28 +0700

    ,

--- 0. Project Final/5. Palletizer/PalletizerV9/PalletizerSlave/PalletizerSlave.ino
+++ 0. Project Final/5. Palletizer/PalletizerV9/PalletizerSlave/PalletizerSlave.ino
@@ -2,10 +2,10 @@
 
 #include "StepperSlave.h"
 
-#define SLAVE_ADDR X_AXIS
+#define SLAVE_ADDR Z_AXIS
 
-#define CLK_PIN 10
-#define CW_PIN 11
+#define CLK_PIN 10  // 10
+#define CW_PIN 11   // 11
 #define RX_PIN 8
 #define TX_PIN 9
 #define SENSOR_PIN 6
@@ -34,10 +34,10 @@
 #define BRAKE_RELEASE_DELAY NO_DELAY
 #endif
 
-// Command reference: x;1;100;2000;d2000;500;1000
-// CMD_RUN      = 1 // x;1;200    - Start movement to position 200
-// CMD_ZERO     = 2 // x;2        - Home the axis
-// CMD_SETSPEED = 6 // x;6;2000   - Set speed to 2000
+// Command reference: z;1;100;2000;d2000;500;1000
+// CMD_RUN      = 1 // z;1;200    - Start movement to position 200
+// CMD_ZERO     = 2 // z;2        - Home the axis
+// CMD_SETSPEED = 6 // z;6;2000   - Set speed to 2000
 
 StepperSlave slave(
   SLAVE_ADDR,

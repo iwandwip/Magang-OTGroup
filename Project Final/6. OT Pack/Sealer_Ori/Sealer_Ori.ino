cf11fa1a6954de0ce1c4bd2631db455a372a2de9
Iwan Dwi Purwanto <97489791+iwandwip@users.noreply.github.com>
2025-07-31 09:31:58 +0700

    .

--- Project Final/6. OT Pack/Sealer_Ori/Sealer_Ori.ino
+++ Project Final/6. OT Pack/Sealer_Ori/Sealer_Ori.ino
@@ -9,34 +9,34 @@ const byte Clockwise = 8;
 // Motor Configuration
 const int MicrosteppingResolution = 4;
 AccelStepper stepper(AccelStepper::DRIVER, P2_Output, Clockwise);
-int Steps = 58*MicrosteppingResolution; 
+int Steps = 58 * MicrosteppingResolution;
 bool state = false;
 
 void setup() {
   pinMode(P1_Input, INPUT_PULLUP);
   pinMode(P2_Output, OUTPUT);
   pinMode(Enable, OUTPUT);
-  pinMode(Clockwise, OUTPUT); 
+  pinMode(Clockwise, OUTPUT);
 }
 
 void loop() {
-  if (digitalRead(P1_Input) == HIGH && !state) { 
-    stepper.setMaxSpeed(1200.0*MicrosteppingResolution); 
-    stepper.setAcceleration(600.0*MicrosteppingResolution);
+  if (digitalRead(P1_Input) == HIGH && !state) {
+    stepper.setMaxSpeed(1200.0 * MicrosteppingResolution);
+    stepper.setAcceleration(600.0 * MicrosteppingResolution);
     delay(150);
     stepper.move(Steps);
-    digitalWrite(Enable, HIGH); 
-    stepper.runToPosition(); 
+    digitalWrite(Enable, HIGH);
+    stepper.runToPosition();
     state = true;
   }
-  if (digitalRead(P1_Input) == LOW && state) { 
-    stepper.setMaxSpeed(3000.0*MicrosteppingResolution); 
-    stepper.setAcceleration(1900.0*MicrosteppingResolution);
+  if (digitalRead(P1_Input) == LOW && state) {
+    stepper.setMaxSpeed(3000.0 * MicrosteppingResolution);
+    stepper.setAcceleration(1900.0 * MicrosteppingResolution);
     delay(250);
-    stepper.move(-Steps+2);
+    stepper.move(-Steps + 2);
     stepper.runToPosition();
     delay(100);
-    digitalWrite(Enable, LOW); 
+    digitalWrite(Enable, LOW);
     state = false;
   }
 }
